16b502ad777b27537184eaf00379c95786a6d2f0
Bert <hagen900@gmail.com>
2026-05-14 22:09:55 -0400

    Add Google Analytics GA4 tracking

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,20 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider afterSignInUrl="/dashboard" afterSignUpUrl="/dashboard">
       <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+        <head>
+          <Script
+            src="https://www.googletagmanager.com/gtag/js?id=G-DCHE6BN1FD"
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-DCHE6BN1FD');
+            `}
+          </Script>
+        </head>
         <body className="min-h-full flex flex-col">{children}</body>
       </html>
     </ClerkProvider>
